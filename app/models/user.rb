c5ae3ac388584aec1c099e4fc0777a1b7fa7542e
Patricio Bruna <pbruna@itlinux.cl>
2011-03-10 21:39:33 -0300

    Se crea partial _footer para correos electrónicos.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,18 @@ class User < ActiveRecord::Base
   acts_as_authentic do |c|
     c.validations_scope = :account_id
   end
-  
+
   validates_presence_of :password
+
+  def before_destroy
+    raise "Admin can no be deleted" if account_admin?
+  end
 
   def deliver_password_reset_instructions!
     reset_perishable_token!
     UserMailer.deliver_password_reset_instructions(self)
   end
-  
+
   def account_admin?
     id == account.admin_id ? true : false
   end
