c61717803ee54317ce7c61821a4e2ebbee01021c
Patricio Bruna <pbruna@gmail.com>
2012-03-01 11:51:04 -0300

    New login and password recovery UI

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -24,8 +24,7 @@ class UserSessionsController < ApplicationController
 
   def destroy
     current_user_session.destroy
-    flash[:notice] = "Ha salido correctamente de Folio."
-    redirect_back_or_default new_user_session_url
+    redirect_back_or_default login_url
   end
 
 end
