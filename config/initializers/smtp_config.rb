cacb63f5e3bf882648f081123affa236d7a1ef5a
Patricio Bruna <pbruna@gmail.com>
2012-03-24 16:45:00 -0300

    Config file for SMTP

---
+++ config/initializers/smtp_config.rb
@@ -0,0 +1,7 @@
+ActionMailer::Base.smtp_settings = {
+  :address        => "lisa.itlinux.cl",
+  :port           => 587,
+  :domain	  => "folio.cl",
+  :user_name	  => "pbruna@it-linux.cl",
+  :password	  => "p0rl4splum4s"
+}
